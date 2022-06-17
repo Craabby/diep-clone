5c51700411eda3c29ea6739057864bfc23800451
Craabby <69945662+Craabby@users.noreply.github.com>
2022-06-16 21:19:49 -0400

    define destructor

--- Shared/src/EntityComponentSystem/Entity.cc
+++ Shared/src/EntityComponentSystem/Entity.cc
@@ -1,9 +1,26 @@
 #include <EntityComponentSystem/Entity.hh>
 
+#include <EntityComponentSystem/Component/Basic.hh>
+#include <EntityComponentSystem/Component/Physics.hh>
+#include <EntityComponentSystem/Component/Types.hh>
+
+template <shared::ecs::component::types::component Component>
+inline void DeleteComponent(shared::ecs::Entity *entity)
+{
+    if (entity->Has<Component>())
+        delete &entity->Get<Component>();
+}
+
 namespace shared::ecs
 {
     Entity::Entity(Simulation *simulation)
         : simulation(simulation)
     {
+    }
+
+    Entity::~Entity()
+    {
+        DeleteComponent<component::Basic>(this);
+        DeleteComponent<component::Physics>(this);
     }
 }
